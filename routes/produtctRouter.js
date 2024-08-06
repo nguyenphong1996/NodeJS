00a24425920e83d7f972c8417cc263eb7a8f1fee
phongnguyen1996 <phongndbps35129@fpt.edu.vn>
2024-08-06 08:54:47 +0700

    swagger

--- routes/produtctRouter.js
+++ routes/produtctRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var modelProduct = require('../models/product');
 var upload = require('../others/upload')
+var email = require('../others/config')
 
 //Lấy danh sách sản phẩm 
 router.get('/list', async function (req, res, next) {
@@ -107,4 +108,22 @@ router.post('/upload', [upload.single('image')],
         }
     });
 
+    router.post("/send-mail", async function(req, res, next){
+        try{
+          const {to, subject, content} = req.body;
+      
+          const mailOptions = {
+            from: "phongnguyen <phongps35129@fpt.edu.vn>",
+            to: to,
+            subject: subject,
+            html: content
+          };
+          await sendMail.transporter.sendMail(mailOptions);
+          res.json({ status: 1, message: "Gửi mail thành công"});
+        }catch(err){
+          res.json({ status: 0, message: "Gửi mail thất bại"});
+        }
+      });
+      
+      
 module.exports = router;
